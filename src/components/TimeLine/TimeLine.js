db59586f6b2e79bff5a095c820469691b6c6dc14
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2021-08-30 13:22:30 +0100

    Tried moving logo.

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -43,7 +43,7 @@ const Timeline = () => {
   }, []);
 
   return (
-    <Section id="about">
+    <Section style={{backgroundColor: 'white'}} id="about">
       <SectionTitle>About Me</SectionTitle>
       {/* <img src = "../../../public/images/5.jpg"/> */}
       <SectionText>
